244b240bf2653e1f7c76c905132aa0dab39f8277
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2024-06-26 21:18:51 -0400

    add header. contact and contact list ocmponent

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="ui fixed menu">
+      <div className="ui container center">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
